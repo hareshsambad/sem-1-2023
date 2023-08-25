2a5490713a035502cb833dad797418e23f09214e
hareshsambad <140933754+hareshsambad@users.noreply.github.com>
2023-08-25 09:41:52 +0530

    Add files via upload

---
+++ op of 2 no as per user choice.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+	int a,b,c;
+	printf("enter value of a =");
+	scanf("%d",&a);
+	printf("enter value of b =");
+	scanf("%d",&b);
+	char op;
+	printf("enter value of op =");
+	scanf("\n%c",&op);
+	if(op=='+'){
+		c=a+b;
+       printf("%d",c);
+       
+	}
+	else if(op=='-'){
+		c=a-b;
+	   printf("%d",c);	
+	}else if(op=='*'){
+		c=a*b;
+		printf("%d",c);
+	}else if(op=='/'){
+		c=a/b;
+		printf("%d",c);
+	}else{printf("invalide op");
+	}
+	}
