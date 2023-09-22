f33510fa0d6fc32a0256f8378eedfe0ad9624069
hareshsambad <140933754+hareshsambad@users.noreply.github.com>
2023-09-22 09:31:20 +0530

    Add files via upload

---
+++ character and vovel are consonant.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+	char temp;
+	printf("enter temp value =");
+	scanf("%c",&temp);
+	if(temp=='a' || temp=='e' || temp=='i' || temp=='o' || temp=='u'){
+	printf("value is vovel");
+	}else{
+	printf("value is consont");
+	}
+	}
---
+++ simple addition .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter value of number a = ");
+    scanf("%d",&a);
+    printf("enter value of number b = ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("%d",c);
+    return 0;
+}
